47b342d281b65ac82d42d52e2dd45f45b6e41c3b
jeromeyers <jeromeyers@hotmail.com>
2013-10-06 12:29:37 -0500

    fixed neighbors

--- app/amorphous.js
+++ app/amorphous.js
@@ -89,7 +89,7 @@ var Amorphous = window.Amorphous = (function() {
                 var context = self.topology.getContext();
                 _(self.particles).each(function(particle) {
                     context.fillStyle = self.regionColors[u.existy(particle.temporaryRegionId) ? particle.temporaryRegionId : (particle.regionId || 1)];
-                    context.fillRect(particle.x, particle.y, 2, 2);
+                    context.fillRect(particle.x, particle.y, 4, 4);
                     if (u.existy(particle.temporaryRegionId)) particle.temporaryRegionId = null;
                 });
             }
@@ -120,12 +120,18 @@ var Amorphous = window.Amorphous = (function() {
                     }
                 };
                 _(self.particles).each(function(particle) {
-                    var y = { radius: 1, found: 0, offset: 1, ticktock: 1, x: particle.x, y: particle.y, direction: 'right', ops:['right', 'down', 'left', 'up'], timesDirectionCalled: 0 };
+                    var y = { found: 0, offset: 1, ticktock: 1, x: particle.x, y: particle.y, direction: 'right', ops: ['right', 'down', 'left', 'up'], timesDirectionCalled: 0 };
                     u.giveYtoXuntilZisTrue(y, function(y) {
                         // mutate x,y by moving in direction but don't do so more than offset number of times
                         directions[y.direction](y);
                         y.timesDirectionCalled += 1;
                         if (y.offset === y.timesDirectionCalled) {
+                            // do ticktocking
+                            if (y.ticktock === 1) y.ticktock = 2;
+                            else {
+                                y.offset += 1;
+                                y.ticktock = 1;
+                            }
                             directions.changeDirection(y);
                         }
 
@@ -138,12 +144,6 @@ var Amorphous = window.Amorphous = (function() {
                             }
                         }
 
-                        // do ticktocking
-                        if (y.ticktock === 1) y.ticktock = 2;
-                        else {
-                            y.offset += 1;
-                            y.ticktock = 1;
-                        }
                     }, function(y) {
                         // loop through until we've found as many neighbors as we want
                         if (y.found >= self.particleParameters.neighbors || (y.x > self.topologyOptions.width && y.y < self.topologyOptions.height)) {
@@ -152,28 +152,9 @@ var Amorphous = window.Amorphous = (function() {
                         return false;
                     });
                 });
-
-
-                //                function isNeighbor(particle1, particle2, radius) {
-                //                    if (particle2.x < (particle1.x + radius) && particle2.x > (particle1.x - radius)) {
-                //                        if (particle2.y < (particle1.y + radius) && particle2.y > (particle1.y - radius)) {
-                //                            return true;
-                //                        }
-                //                    }
-                //                    return false;
-                //                }
-                //                _(self.particles).each(function(particle) {
-                //                    _(self.particles).each(function(innerparticle) {
-                //                        if (particle.id !== innerparticle.id) {
-                //                            if (isNeighbor(particle, innerparticle))  {
-                //                                particle.neighbors.push(innerparticle);
-                //                            }
-                //                        }
-                //                    });
-                //                });
             }
             initNeightbors();
-
+            
             function glimmerNeighbors() {
                 var iterations = _(self.particles).keys().length;
                 u.spaceOutSoManyRunsOf(100, iterations, function(index) {
